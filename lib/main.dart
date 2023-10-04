7b478282023ead9d5f3f304f8ed9490848a81292
YoanMen <yoan.menard.dev@gmail.com>
2023-10-04 18:39:02 +0200

    correct main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:recurring_alarm/domain/notification_manager.dart';
 import 'package:recurring_alarm/routing/app_routes.dart';
 import 'package:recurring_alarm/theme/custom_theme.dart';
 import 'package:timezone/data/latest.dart' as tz;
